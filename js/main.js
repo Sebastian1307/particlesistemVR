1959718fd495c3aa66dcbe7b8378ec773a549024
Sebastian1307 <56195983+Sebastian1307@users.noreply.github.com>
2023-11-14 09:56:17 -0500

    Update main.js 2

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const colorbg = new THREE.Color(0x0000ff);
 scene.background = colorbg;
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 30;
+camera.position.z = 20;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
